7abd8b7ca41904df34cf4cc8b791a7cceb47dd19
Dmitriy <dimakrytoytshel@gmail.com>
2024-08-30 22:36:19 +0300

    secondP

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true, // Это добавит слэш в конце URL, что полезно для GitHub Pages
-  assetPrefix: process.env.GITHUB_PAGES ? '/your-repo-name/' : '', // Замените 'your-repo-name' на имя вашего репозитория
-  // Другие настройки по необходимости
+  trailingSlash: true,
+  assetPrefix: process.env.GITHUB_PAGES ? '/buzzenglish' : '', 
 };
 
 export default nextConfig;
